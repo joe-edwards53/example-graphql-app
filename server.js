e279f0667e2eb05ab2fe46a12d53d9cb5113c54f
Joe Edwards <joseph.edwards@robinpowered.com>
2022-09-21 10:26:29 -0700

    Add minimal func. anime graphql application

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
+} = require('graphql')
+const app = express()
+
+const characters = [
+	{ id: 1, name: 'Light' },
+    { id: 2, name: 'L' },
+	{ id: 3, name: 'Eren' },
+	{ id: 4, name: 'Kakashi' },
+    { id: 5, name: 'Near' },
+    { id: 6, name: 'Trunks' },
+    { id: 7, name: 'Naruto' },
+    { id: 8, name: 'Monkey D. Luffy' },
+    { id: 9, name: 'Levi' }
+]
+
+const animes = [
+	{ id: 1, name: 'Deathnote', characterId: 1 },
+	{ id: 2, name: 'Deathnote', characterId: 2 },
+	{ id: 3, name: 'AOT', characterId: 3 },
+	{ id: 4, name: 'Naruto', characterId: 4 },
+	{ id: 5, name: 'Deathnote', characterId: 5 },
+	{ id: 6, name: 'DBZ', characterId: 6 },
+	{ id: 7, name: 'Naruto', characterId: 7 },
+	{ id: 8, name: 'One Piece', characterId: 8 },
+    { id: 8, name: 'AOT', characterId: 9 }
+]
+
+const CharacterType = new GraphQLObjectType({
+    name: 'Character',
+    description: 'This reps a Character in a Anime',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+        name: { type: new GraphQLNonNull(GraphQLString) }
+    })
+})
+
+const AnimeType = new GraphQLObjectType({
+    name: 'Anime',
+    description: 'This reps an Anime which has this Character',
+    fields: () => ({
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        characterId: { type: new GraphQLNonNull(GraphQLInt) },
+        character: { 
+            type: CharacterType,
+            resolve: (anime) => {
+                return characters.find(character => character.id === anime.characterId)
+            }
+        }
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        animes: {
+            type: new GraphQLList(AnimeType),
+            description: 'List of All Animes',
+            resolve: () => animes
+        }
+    })
+})
+ 
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
+app.listen(5000., () => console.log('Server running'))
